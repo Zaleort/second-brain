4f3db0e077a87843250f9df5882ae7623c92c2ea
Alexandre Henrique <zaleortgames@outlook.com>
2022-09-13 10:51:45 +0200

    Handle exceptions at CreateCategory

--- src/Categories/Application/CreateCategory/CreateCategoryHandler.php
+++ src/Categories/Application/CreateCategory/CreateCategoryHandler.php
@@ -4,6 +4,7 @@ namespace App\Categories\Application\CreateCategory;
 
 use App\Categories\Domain\Category;
 use App\Categories\Domain\CategoryRepositoryInterface;
+use App\Categories\Domain\ForbiddenNameException;
 use App\Categories\Domain\TestService;
 
 class CreateCategoryHandler
@@ -12,8 +13,17 @@ class CreateCategoryHandler
     {
     }
 
+    /**
+     * @throws CategoryAlreadyExistsException
+     * @throws ForbiddenNameException
+     */
     public function execute(CreateCategoryCommand $command): void
     {
+        $categoryExistent = $this->categoryRepository->findByName($command->name);
+        if ($categoryExistent) {
+            throw new CategoryAlreadyExistsException('La categoría ya existe');
+        }
+
         $category = Category::create($command->name);
         $this->categoryRepository->save($category);
     }
--- src/Categories/Domain/Category.php
+++ src/Categories/Domain/Category.php
@@ -4,13 +4,27 @@ namespace App\Categories\Domain;
 
 class Category
 {
-    public function __construct(private readonly string $name)
+    public function __construct(private readonly string $name, private readonly ?int $id = null)
     {
     }
 
+    /**
+     * @throws ForbiddenNameException
+     */
     public static function create(string $name): self
     {
+        if ($name === 'Brokalia') {
+            throw new ForbiddenNameException('El nombre no puede contener Brokalia');
+        }
         return new self($name);
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
     }
 
     /**
