5357e764f34ff9514c7996a1111c532dc4c34271
Alexandre Henrique <zaleortgames@outlook.com>
2022-09-20 08:50:09 +0200

    Fix: CategoryName

--- src/Categories/Domain/CategoryName.php
+++ src/Categories/Domain/CategoryName.php
@@ -9,14 +9,12 @@ class CategoryName extends StringValueObject
     /**
      * @throws ForbiddenNameException
      */
-    private function __construct(public readonly string $value)
+    private function __construct(string $value)
     {
         parent::__construct($value);
         if ($value === 'Brokalia') {
             throw new ForbiddenNameException('El nombre no puede contener Brokalia');
         }
-
-
     }
 
     /**
